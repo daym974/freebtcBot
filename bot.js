988240ef0597d379377f19d7d7d1c9aaba9f1a80
daym974 <tftdaym974@gmail.com>
2016-12-19 03:05:24 +0100

    0.3 is out!
    Do not abuse.

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Autobet
-// @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  try to take over the world!
+// @namespace    Autobet script
+// @version      0.3
+// @description  freebitco.in auto bet player. 
 // @author       Daym
 // @match        https://freebitco.in/?op=home
 // @grant        none
 // ==/UserScript==
 
+
+var playing = true; //savoir si autobet tourne ou pas
 
 /**
 switchBet
@@ -15,7 +17,8 @@ switchBet
 Coche le bet choisi
 */
 function switchBet(value){
-	$('#autobet_bet_' + value).get(0).click();
+    $('#autobet_bet_' + value).get(0).click();
+    console.log("Bet switched to " + value + "\n");
 }
 
 /**
@@ -24,79 +27,155 @@ Click sur le bouton 'START_AUTO_BET'
 */
 function startBet(){
 
-	$('#start_autobet').get(0).click();
+    $('#start_autobet').get(0).click();
+    console.log("Bet has started\n");
 }
 
 
 /**
 Change la valeur max
-@param Float value 
+@param Float value
 */
 function changeMaxBet(value){
-	$('#autobet_max_bet').val(value);
+    $('#autobet_max_bet').val(value);
+    console.log("Max bet switched to " + value + "\n");
 }
 
 
 /**
 Passage vers augmentation du bet de 100% en cas de loose
-
 */
 function onLoseConf(){
-	$('#autobet_lose_increase_bet_percent').val(100);
-	$('#autobet_lose_increase_bet').get(0).click();
+    $('#autobet_lose_increase_bet_percent').val(100);
+    $('#autobet_lose_increase_bet').get(0).click();
+    console.log("Config on lose done\n");
 }
 
 /**
 Change la valeur du pari
-@param Float value 
+@param Float value
 */
 function changeBet(value){
-	$('#autobet_base_bet').val(value);
+    $('#autobet_base_bet').val(value);
+    console.log("Bet changed to " + value + "\n");
 }
 
 /**
 Change le nombre de répétitions
-@param Float value 
+@param Float value
 */
 function changeRollCount(value){
-$('#autobet_roll_count').val(value);
+    $('#autobet_roll_count').val(value);
+    console.log("Roll count set to " + value +"\n");
+}
+
+/**
+Retourne la balance actuelle
+*/
+function getBalance(){
+    return parseFloat($('#balance').text());
+}
+
+/**
+Retourne la valeur actuelle du bet
+*/
+function getBet(){
+    return parseFloat($('#autobet_base_bet').val());
+}
+
+/**
+Calcule le max de répétitions possibles en fonction de la balance
+*/
+function maxRolls(){
+    var balance = getBalance();
+    var bet = getBet();
+    var count = 0;
+
+    while(bet < balance){
+        bet *= 2;
+        count+=1;
+    }
+    count = parseInt(count/3);
+    console.log("Max rolls: " + count + "\n");
+    if(count < 20){
+        alert("Not enough funds to use autobet! You need to reach 20 rolls at least!");
+        playing = false;
+    }
+    return count;
 }
 
 
 /**
-main
-Déroulement des fonctions
+Procède aux calculs nécessaires pour démarrer autobet
 */
-function main(){
+function init(){
+    $(document).ready(function(){
+        onLoseConf();
+    });
+    changeRollCount(maxRolls());
+}
 
-var counter = 0;
-
-changeRollCount(20);
-switchBet('alternate');
-$(document).ready(function(){
-	onLoseConf();
-});
-
-setInterval(function(){ 
-
-	if(counter < 300000){
-		
-	if(counter < 100000){
-		switchBet('hi');
-	}else if(100000 < counter <200000){
-		switchBet('lo');
-	}else
-		switchBet('alternate');
-
-
-		$('#start_autobet').get(0).click();
-		counter+=6500;
-	}else{
-		alert('Game finished. Please wait before relauching autobet...');
-	}
-},6500);
+/**
+Choisit aléatoirement le type de bet
+*/
+function randomBet(){
+    var min = 1;
+    var max = 10;
+    var res = parseInt(Math.random() * (max - min) + min);
+    if(res % 2 == 0){
+        switchBet('alternate');
+    }else if(res % 3 == 0){
+        switchBet('lo');
+    }else{
+        switchBet('hi');
+    }
 }
 
 
-main();
 
+/**
+startAutobet
+Déroulement des fonctions
+*/
+function startAutobet(){
+    var balanceDebut = getBalance(); //pour avoir les profits après
+    var counter = 0; //timer pour l'intervalle
+    var remaining = 0; //restant
+    var attente = 1000;
+    init();
+
+    console.log("Balance: " + getBalance() + "\n");
+
+
+    setInterval(function(){
+        remaining = parseInt($('#rolls_remaining_count').text());
+        if(playing && remaining == 0){
+            if(counter < 300000){
+                randomBet();
+                startBet();
+                counter+=6500;
+            }else{
+                playing = false;
+                alert('Game finished. Please wait before relauching autobet...');
+                console.log("Balance: " + getBalance() + "\n");
+                var balanceFin = getBalance();
+                var profit = balanceFin - balanceDebut;
+                console.log("Profit: " + profit + "\n");
+            }
+        }else if(remaining != 0){
+            attente+=1000;
+        }
+    },attente);
+
+
+}
+
+/**
+
+  /''''''\
+~|Démarrage''>
+   (.)   (.)
+
+*/
+if(confirm("Do you want to start autobet?"))
+    startAutobet();
